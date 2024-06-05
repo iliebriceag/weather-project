06cec81695d7f2a7215be7647c1ac553078f58b7
Teri01 <briceag.ilie97@gmail.com>
2024-06-05 10:39:31 +0300

    finish

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,11 +1,11 @@
 var city = 'București'
-var apiKey = 'cc954e6db559314fdca3a84daab3a4cc'
-var unitsKey = 'metric'
+var key = 'cc954e6db559314fdca3a84daab3a4cc'
+var units = 'metric'
 var language = 'ro'
 
 function currentWeatherEndpoint(city) {
-    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&units=${unitsKey}&appid=${apiKey}`
+    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${language}&units=${units}&appid=${key}`
 }
 function forecastURL(city) {
-    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${language}&units=${unitsKey}&appid=${apiKey}`
+    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${language}&units=${units}&appid=${key}`
 }
